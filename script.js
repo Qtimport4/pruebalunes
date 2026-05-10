ac38c25249fc1d99671861bd9c47078ab8ef3bb0
Qtimport4 <martin.zapata02@inacapmail.cl>
2026-05-09 21:41:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 function toggleTexto(num) {
+
     var texto = document.getElementById("texto" + num);
 
-    if (texto.style.display === "none" || texto.style.display === "") {
-        texto.style.display = "block";
-    } else {
-        texto.style.display = "none";
-    }
+    texto.classList.toggle("activo");
+
 }
